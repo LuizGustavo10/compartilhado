06835abbf3e24d0a4025f54f5c37dd48a70fd47a
ANGELO PICHITELLI ALVES DA SILVA <angelo.6973@aluno.pr.senac.br>
2024-07-26 17:36:17 -0300

    Made by Angelo

---
+++ Angelo/Angelo/script.js
@@ -0,0 +1,18 @@
+function buscarCep(){
+    //pegar o cep no input do html
+    let cep = document.getElementById('cep').value;
+    //link da API
+    let url = 'https://viacep.com.br/ws/'+cep+'/json/';
+//buscar url e espera um arquivo json, em seguida armazena em dados
+    fetch(url)
+    .then(response => response.json())
+    .then(dados => {
+        document.getElementById('resultado').innerHTML = 
+        `<strong> Cidade: </strong> ${dados.localidade} <br>
+        <strong> Logradouro: </strong> ${dados.logradouro} <br>
+        <strong> DDD: </strong> ${dados.ddd}`;
+
+    }).catch(error => alert('Deu errado!' + error))
+
+}
+
