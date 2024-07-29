d0d3147460d97e26ad8384288a3e85de07199fac
luizGustavo10 <gustavo180g@gmail.com>
2024-07-29 17:27:36 -0300

    cep luiz

--- Luiz/script.js
+++ Luiz/script.js
@@ -1,8 +1,12 @@
+var mapa = '';
+
+
 function buscar(){
     //pegar o cep no input do html
     let cep = document.getElementById('cep').value;
     //link da API
     let url = 'https://viacep.com.br/ws/'+cep+'/json/';
+
     //buscar url e espera um arquivo json, em seguida armazena em dados
     fetch(url)
     .then(response => response.json())
@@ -11,8 +15,20 @@ function buscar(){
         `<strong> Cidade: </strong> ${dados.localidade} <br>
          <strong> Logradouro: </strong> ${dados.logradouro} <br>
          <strong> DDD: </strong> ${dados.ddd} <br>
+         <strong> CEP: </strong> ${dados.cep} <br>
+         <strong> Bairro: </strong> ${dados.bairro} <br>
+         <strong> Estado: </strong> ${dados.uf} <br>
+         <strong> Ibge: </strong> ${dados.ibge} <br>
+
          ` ;
+
     
     }).catch(error => alert('Deu errado! ' + error))
 
+}
+
+function buscarMapa(){
+    let cep = document.getElementById('cep').value;
+    var mapa = "https://www.google.com/maps/place/"+cep;
+    window.open(mapa, '_blank');
 }
