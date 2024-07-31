4f6205a33c5336c566a3adecb850a3b6c978f42a
JOSE APARECIDO INACIO DOS SANTOS <jose.05906@aluno.pr.senac.br>
2024-07-31 15:11:23 -0300

    pokedex

--- jose ap/pokeapi/script.js
+++ jose ap/pokeapi/script.js
@@ -1,27 +1,47 @@
-function buscar(){
-let entrada = document.getElementById("entrada").value.toLowerCase();
-let url = `https://pokeapi.co/api/v2/pokemon/${entrada}`;
 
-//buscar url - response é a resposta, que espera o retorno de um arquivo json
+let contador = 0;
 
-fetch(url)
-.then(response =>response.json())
-.then(dados => {
-alert(dados.name);
-//pega o elemento tela no html
-let tela  = document.getElementById("tela");
+ function buscar() {
 
-//faz aparecer no elemento tela
-tela.innerHTML =
+    let entrada = document.getElementById("entrada").value.toLowerCase();
+    //se o tamanho da entrada for menor que 1 caracter
+if(entrada.length < 1 ){
+    entrada = contador;
+}
+    let url = `https://pokeapi.co/api/v2/pokemon/${entrada}`;
+
+    //buscar url - response e a resposta ,  espera e retorna  de um arquivo json
+    fetch(url)
+        .then(response => response.json())
+        .then(dados => {
+//pega o elemento tela no html 
+ let tela = document.getElementById("tela");
+ //faz parecer no elemento tela
+ tela.innerHTML =
 
 `
 <img src="${dados.sprites.front_default}">
 <img src="${dados.sprites.back_default}">
-<h2>nome: ${dados.name} </h2>
-<p> id: ${dados.id}</p>
+<h2>nome: ${dados.name}</h2>
+<p> id: ${dados.id} </p>
+<p> tipo: ${dados.types.map(type => type.type.name)} </p>
+<p> Habilidades: ${dados.abilities.map(ability => ability.ability.name)} <p>
 
-`;
-});
 
+` ;
+contador = dados.id;
+//limpar campo de entrada
+document.getElementById("entrada").value="";
+        });
+    }
 
+    function avancar(){
+contador = contador +1;
+buscar();
+
+    }
+
+    function voltar(){
+    contador-=1;
+    buscar();
 }
