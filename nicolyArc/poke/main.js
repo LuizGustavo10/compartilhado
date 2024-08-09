592c4be4c1d3d52f63b1201ce3a5836a20bf3764
nico-cbr <ccarvalhonicoly@gmail.com>
2024-08-09 15:46:31 -0300

    add

--- nicolyArc/poke/main.js
+++ nicolyArc/poke/main.js
@@ -31,26 +31,60 @@ function buscar(){
     
     
     <div class="informacoes">
-    <h2>Nome: ${dados.name}</h2>
-    <p> id: ${dados.id}</p>
-    <p> Tipo: ${dados.types.map(type => type.type.name)}</p>
-    <p> habilidades: ${dados.abilities.map(ability =>  ability.ability.name)}</p>
-    <p> Vida: ${hp}</p>
-    <p> Ataque: ${attack}</p>
-    <p> Defesa: ${defense}</p>
-    <p> Velocidade: ${speed}</p>
-    <p> Peso: ${dados.weight}</p>
-    <p> Altura: ${dados.height}</p>
+
+    <h2><b>Nome:</b> ${dados.name}</h2>
+    <p> <b>Tipo:</b> ${dados.types.map(type => type.type.name)}</p>
+    <p> <b>habilidades:</b> ${dados.abilities.map(ability =>  ability.ability.name)}</p>
+    <p> <b>Vida:</b> ${hp}</p>
+    <div id="hp"></div>
+    <p> <b>Ataque:</b> ${attack}</p>
+    <div id="ataque"></div>
+    <p> <b>Defesa:</b> ${defense}</p>
+    <div id="defesa"></div>
+    <p> <b>Velocidade:</b> ${speed}</p>
+    <p> <b>Peso:</b> ${dados.weight}</p>
+    <p> <b>Altura:</b> ${dados.height}</p>
     <button class="btn dois" onclick="voltar()"><i class="fa-solid fa-arrow-left"></i></button>
     <button class="btn um" onclick="avancar()"><i class="fa-solid fa-arrow-right"></i></button>
     </div>
     `;
     contador = dados.id;
     entrada = document.getElementById("entrada").value="";
+
+    let maxStat = 255;
+
+    let hpBarra = new ProgressBar.Line('#hp',{ 
+      strokeWidth: 2, //Espessura da linha de progresso
+      color: '#ce5252', // cor da linha de progresso
+      trailColor: '#e2e2e2', // cor da trilha
+      trailWidth: '1', //espessura da trilha
+      duration: 900, // duração da animação
+    });
+
+    let ataBarra = new ProgressBar.Line('#ataque',{ 
+      strokeWidth: 2, //Espessura da linha de progresso
+      color: '#f1ca5f', // cor da linha de progresso
+      trailColor: '#e2e2e2', // cor da trilha
+      trailWidth: '1', //espessura da trilha
+      duration: 900, // duração da animação
+    });
+
+    let defBarra = new ProgressBar.Line('#defesa',{ 
+      strokeWidth: 2, //Espessura da linha de progresso
+      color: '#72c1cc', // cor da linha de progresso
+      trailColor: '#e2e2e2', // cor da trilha
+      trailWidth: '1', //espessura da trilha
+      duration: 900, // duração da animação
+    });
+
+    hpBarra.animate(hp/maxStat)
+    ataBarra.animate(attack/maxStat)
+    defBarra.animate(defense/maxStat)
+
   }).catch(error => {
     tela.innerHTML = 
     `<h2>Pokemom não encontrado</h2>
-    <p>Tente novamente + ${error}</p>`
+    <p>Tente novamente <br> ${error}</p>`
   })
 }
 
